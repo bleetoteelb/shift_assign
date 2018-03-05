455aeecc4799ee4902d017ac819520f2a902246e
bleetoteelb <sh1993park@unist.ac.kr>
2018-03-06 03:54:14 +0900

    middle

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 var http = require('http');
+var path = require('path');
 var express = require('express');
 var app = express();
  
 var server = app.listen(8889, function () {
 	console.log('load success!');
 });
-
+app.use(express.static(path.join(__dirname, '/')));
 app.get('/',function(req,res){
 	res.sendFile(__dirname + '/index.html');
 });
